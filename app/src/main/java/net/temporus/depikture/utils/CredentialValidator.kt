34b01f23968e15cfaa99518a0decccc6b61f25bd
Jonas <thenixchecker@gmail.com>
2017-12-08 18:21:58 +0100

    Lowered required password length for testing purposes

--- app/src/main/java/net/temporus/depikture/utils/CredentialValidator.kt
+++ app/src/main/java/net/temporus/depikture/utils/CredentialValidator.kt
@@ -5,4 +5,4 @@ fun isNameValid(name: String): Boolean = name.length > 3 && name.trim() != ""
 fun isEmailValid(email: String): Boolean = email.length > 6 && email.trim() != ""
 
 fun isPasswordValid(password: String): Boolean =
-        password.length > 7 && password.trim() != ""
+        password.length > 3 && password.trim() != ""
